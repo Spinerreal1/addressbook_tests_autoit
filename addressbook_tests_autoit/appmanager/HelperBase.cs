8468b7d51ba23bd5fbd03d6bdc9d8a33185aec86
Дмитрий Иванов <randomsize@narod.ru>
2022-08-07 17:15:11 +0400

    Add project files.

---
+++ addressbook_tests_autoit/appmanager/HelperBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AutoItX3Lib;
+
+
+namespace addressbook_tests_autoit
+{
+    public class HelperBase
+    {
+        protected string WINTITLE;
+        protected ApplicationManager manager;
+        protected AutoItX3 aux;
+
+        public HelperBase(ApplicationManager manager)
+        {
+            this.manager = manager;
+            this.aux = manager.Aux;
+            WINTITLE = ApplicationManager.WINTITLE;
+        }
+    }
+}
